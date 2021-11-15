f1567cc96482664e0c5fe580befb237f25bd3e5b
Nandini S <68180177+NANDINIS123@users.noreply.github.com>
2021-11-15 16:47:21 +0530

    Delete SpringCoreAssignment directory

--- SpringCoreAssignment/SpringCoreAssignment_3/src/main/java/SpringCoreAssignment_3/BankAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package SpringCoreAssignment_3;
-
-public interface BankAccountRepository 
-{
-	 public double getBalance(long accountld);
-	 public double updateBalance (long accountld, double newBalance);
-}
